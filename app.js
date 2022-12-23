5a4614b259470bb5faae24607de2fca3041fd706
Nelson Saraiva <nsaraiva@gmail.com>
2022-12-23 01:53:17 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { VerifyDiscordRequest } from "./discord/discord.utils.js";
+import { TEST_COMMAND, HasGuildCommands } from "./discord/discord.commands.js";
+import {
+    InteractionType,
+    InteractionResponseType,
+    InteractionResponseFlags,
+    MessageComponentTypes,
+    ButtonStyleTypes,
+  } from "discord-interactions";
+import dotenv from "dotenv";
+dotenv.config()
+
+// Create an express app
+const app = express();
+// Get port, or default to 3000
+const port = process.env.PORT || 3000;
+
+// Parse request body and verifies incoming requests using discord-interactions package
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+
+
+app.post("/interactions", async function (req, res){
+    const { type, id, data} = req.body;
+
+    // Handle verification requests
+    if (type === InteractionType.PING) {
+        res.send({ type: InteractionResponseType.PONG });
+    }
+
+    // Handle slash command requests
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name} = data;
+
+        // "test" guild command
+        if (name === "test") {
+            res.send({ type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: "Hello World!",
+                },
+            });
+        }
+    }
+});
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}`);
+
+    // Check if guild commands from commands.json are installed (if not, install them)
+    HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [TEST_COMMAND]);
+});
